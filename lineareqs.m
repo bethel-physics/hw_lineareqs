a1b7e99666f5e425bb71852820a8e86190e3b4fb
Julie Hogan <jmhogan@fnal.gov>
2018-03-17 08:15:31 -0500

    Create lineareqs.m

---
+++ lineareqs.m
@@ -0,0 +1,16 @@
+%% Program to solve linear equations for HW 7
+
+%% Problem 1 -- write and solve the linear equations for the coupled springs
+m = 0.5;   % kg
+L = 0.25;  % m
+k = 100;   % N/m
+
+
+%% Problem 2 -- write and solve the linear equations for circuit
+R1 = 1;  % Ohms
+R2 = 1;
+R3 = 2;
+R4 = 2;
+R5 = 5;
+V1 = 2;  % Volts
+V3 = 5;
