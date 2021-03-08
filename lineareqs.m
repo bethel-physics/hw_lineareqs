daea254e3ba30927c8759a31517f2363ab4bd2db
Julie Hogan <juh82678@bu.ac.bethel.edu>
2021-03-08 16:19:23 -0600

    Add in-class here

--- lineareqs.m
+++ lineareqs.m
@@ -1,6 +1,15 @@
 %% Program to solve linear equations for HW 7
 
 %% Problem 1 -- write and solve the linear equations for the coupled springs
+k1 = 150; % N/m
+k2 = 125; % N/m
+k3 = 100; % N/m
+k4 = 75;  % N/m
+m = 0.5;  % kg
+L = 0.25; % m
+
+
+%% Problem 2 -- add another spring! Use all the same k values this time.
 m = 0.5;   % kg
 L = 0.25;  % m
 k = 100;   % N/m
